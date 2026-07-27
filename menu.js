d0e8ae78d96ea96e9948386eba7997682902d825
4450dantez <dandantez45@gmail.com>
2026-07-27 22:40:55 +0300

    Removed command descriptions from menu list

--- menu.js
+++ menu.js
@@ -7,22 +7,23 @@ function getMenu(prefix, pushName) {
 ⚙️ *Prefix:* [ ${prefix} ]
 
 ─── 🌐 *GENERAL COMMANDS* ───
-• *${prefix}ping* - Check bot response speed
-• *${prefix}menu* - Display this command menu
-• *${prefix}owner* - Contact bot owner
-• *${prefix}runtime* - Display bot uptime and system specs
+• *${prefix}ping*
+• *${prefix}menu*
+• *${prefix}owner*
+• *${prefix}runtime*
 
 ─── 🎨 *MEDIA & UTILS* ───
-• *${prefix}sticker* - Convert reply/caption image to sticker
-• *${prefix}say <text>* - Make the bot repeat your text
-• *${prefix}quote* - Get a random inspirational quote
-• *${prefix}joke* - Get a random funny joke
+• *${prefix}sticker*
+• *${prefix}say*
+• *${prefix}quote*
+• *${prefix}joke*
 
 ─── 👥 *GROUP COMMANDS* ───
-• *${prefix}tagall* - Mention all group members
-• *${prefix}groupinfo* - Show details about current group
+• *${prefix}tagall*
+• *${prefix}groupinfo*
 
 💡 *Tip:* Send *${prefix}sticker* as caption to an image to convert it!`;
 }
 
 module.exports = { getMenu };
+
